f3ea5af7774116de2e484c7c74b8f327c46603f5
Syllas Braga <syllasbraga2@gmail.com>
2023-05-02 14:16:31 -0300

    feat: Tabela Cliente na base dados

--- data/data.sql
+++ data/data.sql
@@ -5,5 +5,15 @@ use crud_python;
 create table vendas(
 id_venda int not null primary key auto_increment,
 nome_produto varchar(255) not null,
-valor float(10,2) not null
+valor float(10,2) not null,
+cliente_id int not null
 );
+
+create table cliente(
+id_cliente int not null primary key auto_increment,
+nome_cliente varchar(255) not null,
+login varchar(255) unique not null
+);
+
+alter table vendas
+add foreign key fk_cliente_venda(cliente_id) references cliente(id_cliente)
